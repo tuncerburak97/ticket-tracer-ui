e91c9283f6fdc8d2cc298c7013fb61799e5c7fb7
Burak Tuncer <33039466+tuncerburak97@users.noreply.github.com>
2024-11-26 20:36:41 +0300

    Update api.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,5 +1,5 @@
-//const BASE_URL = "http://35.232.70.133";
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://api.biletio.xyz";
+//const BASE_URL = "http://localhost:8080";
 
 export const API_URL_LOAD = BASE_URL + "/tcdd/load";
 export const API_URL_QUERY = BASE_URL + "/tcdd/query";
